c254696e758cd32a67251220a0b947d3f8f0deb7
OTNT <otnt@users.noreply.github.com>
2016-04-07 00:35:16 -0400

    Update basic_tools.sh

--- install/basic_tools.sh
+++ install/basic_tools.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+Green='\033[0;32m'
+Purple='\033[0;35m'
+NC='\033[0m' # No Color
+
 # install basic tools
 apt-get update -y -qq && \
 apt-get install -y -qq build-essential cmake git python-dev python3-dev wget software-properties-common && \
-echo "Basic tools installed: cmake, git, python, wget, other essential tools"
+echo -e "${Green}Basic tools installed: cmake, git, python, wget, other essential tools${NC}"
 
