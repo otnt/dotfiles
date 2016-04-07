d4cdd990962ed161b1af516c90fdf37757233b93
OTNT <otnt@users.noreply.github.com>
2016-04-06 23:49:38 -0400

    Update java.sh

--- install/java.sh
+++ install/java.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
+Green='\033[0;32m'
+Purple='\033[0;35m'
+NC='\033[0m' # No Color
+
 # Java 8
 add-apt-repository ppa:webupd8team/java -y && \
-apt-get update -y && \
-apt-get install oracle-java8-installer -y && \
-apt-get install oracle-java8-set-default -y
+apt-get update -y -qq && \
+apt-get install oracle-java8-installer -y -qq && \
+apt-get install oracle-java8-set-default -y -qq && \
+echo -e "${Green}Java 8 installed successfully${NC}"
