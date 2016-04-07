8bcb7948c01b69cd808a4fc3ca4ca822860ef6be
OTNT <otnt@users.noreply.github.com>
2016-04-06 22:30:06 -0400

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,9 +1,9 @@
 # Install Node.js, npm, need sudo
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-apt-get install -y nodejs build-essential
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - &&\
+apt-get install -y nodejs build-essential &&\
 # Update npm
-npm install npm -g
+npm install npm -g &&\
 # Update nodejs
-npm cache clean -f
-npm install -g n
+npm cache clean -f &&\
+npm install -g n &&\
 n stable
