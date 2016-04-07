6187568477ecaf17f04dd863c171815c8816e9d2
OTNT <otnt@users.noreply.github.com>
2016-04-06 23:48:38 -0400

    Update golang.sh

--- install/golang.sh
+++ install/golang.sh
@@ -5,7 +5,7 @@ Purple='\033[0;35m'
 NC='\033[0m' # No Color
 
 # Install latest stable golang
-add-apt-repository ppa:ubuntu-lxc/lxd-stable -y -qq &&\
+add-apt-repository ppa:ubuntu-lxc/lxd-stable -y &&\
 apt-get update -y -qq &&\
 apt-get install golang -y -qq && \
 echo -e "${Green}Golang installed successfully${NC}"
