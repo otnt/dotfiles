977a56d5809d289762304b486e77c1c0d8f5d7d6
OTNT <otnt@users.noreply.github.com>
2016-04-06 23:59:37 -0400

    Update python.sh

--- install/python.sh
+++ install/python.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
+Green='\033[0;32m'
+Purple='\033[0;35m'
+NC='\033[0m' # No Color
+
 # Python
-apt-get update -y && \
-apt-get install python-pip -y && \
-apt-get install python-dev python3-dev -y
+apt-get update -y -qq && \
+apt-get install python-pip -y -qq && \
+apt-get install python-dev python3-dev -y -qq && \
+echo -e "${Green}Python installed successfully${NC}"
+
