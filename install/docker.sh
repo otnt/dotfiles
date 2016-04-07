93104dce419dca5c1e96d706b85fea168ca993c9
OTNT <otnt@users.noreply.github.com>
2016-04-06 23:44:19 -0400

    Update docker.sh

--- install/docker.sh
+++ install/docker.sh
@@ -1,10 +1,16 @@
+Green='\033[0;32m'
+Purple='\033[0;35m'
+NC='\033[0m' # No Color
+
 # install docker engine
 apt-get update -y -qq && \
 apt-get install curl -y -qq && \
 curl -fsSL https://get.docker.com/ | sh && \
-echo "docker engine installed successfully"
+usermod -aG docker `whoami` && \
+echo -e "${Green}docker engine installed successfully${NC}" && \
+echo -e "${Purple}add user to docker group: usermod -aG docker USER${NC}"
 
 # install docker machine
 curl -sL https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
 chmod +x /usr/local/bin/docker-machine && \
-echo "docker machine installed successfully"
+echo -e "${Green}docker machine installed successfully${NC}"
