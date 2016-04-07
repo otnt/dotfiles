690fc5a2d6fbcc490d8ab98abf96796c03996ce5
OTNT <otnt@users.noreply.github.com>
2016-04-06 23:15:33 -0400

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -14,17 +14,17 @@ then
 fi
 
 # install basic tools
-apt-get update -y &&\
+apt-get update -y && \
 apt-get install -y build-essential cmake git python-dev python3-dev wget software-properties-common
 
 # install vim
-add-apt-repository ppa:fcwu-tw/ppa -y &&\
-apt-get update -y &&\
+add-apt-repository ppa:fcwu-tw/ppa -y && \
+apt-get update -y && \
 apt-get install vim -y
 
 # We're using the ultimate vim configuration as our base configuration
 # https://github.com/amix/vimrc
-git clone https://github.com/amix/vimrc.git $USERHOME/.vim_runtime &&\
+git clone https://github.com/amix/vimrc.git $USERHOME/.vim_runtime && \
 sh $USERHOME/.vim_runtime/install_awesome_vimrc.sh
 
 # my vim config file, supplementary to ultimate vim
@@ -32,15 +32,15 @@ wget https://raw.githubusercontent.com/otnt/dotfiles/master/my_configs.vim  -O $
 chown -R $USERNAME $USERHOME/.vim_runtime
 
 # install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git $USERHOME/.vim/bundle/Vundle.vim
 
 # We are going to configure YouCompleteMe, which is missing in ultimate vim
 # configuration. Note YouCompleteMe is a little bit tricky, it needs some
 # extra dependency, and also needs recompile each time you want to update it.
-git clone https://github.com/Valloric/YouCompleteMe.git $USERHOME/.vim/bundle/YouCompleteMe &&\
-cd $USERHOME/.vim/bundle/YouCompleteMe &&\
-git submodule update --init --recursive &&\
-cd $USERHOME &&\
+git clone https://github.com/Valloric/YouCompleteMe.git $USERHOME/.vim/bundle/YouCompleteMe && \
+cd $USERHOME/.vim/bundle/YouCompleteMe && \
+git submodule update --init --recursive && \
+cd $USERHOME && \
 mkdir ycm_build
 
 # change mode
@@ -48,29 +48,29 @@ chown -R $USERNAME $USERHOME/.vim_runtime
 chown -R $USERNAME $USERHOME/.vim
 
 # skip this step if you don't need c-family language support
-cd $USERHOME/ycm_build &&\
-mkdir clang++ &&\
-wget http://llvm.org/releases/3.8.0/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz &&\
-tar -xf clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz -C clang++ --strip-components=1 &&\
+cd $USERHOME/ycm_build && \
+mkdir clang++ && \
+wget http://llvm.org/releases/3.8.0/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz && \
+tar -xf clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz -C clang++ --strip-components=1 && \
 rm clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 chown -R $USERNAME $USERHOME/ycm_build
 
-cd $USERHOME/ycm_build &&\
+cd $USERHOME/ycm_build && \
 # if don't need c-family language support
-# cmake -G "Unix Makefiles" . $USERHOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp &&\
+# cmake -G "Unix Makefiles" . $USERHOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp && \
 # otherwise
-cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=$USERHOME/ycm_build/clang++ . $USERHOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp &&\
+cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=$USERHOME/ycm_build/clang++ . $USERHOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp && \
 # build it in both cases
 cmake --build . --target ycm_core
 
 # Golang support
-curl -sL https://raw.githubusercontent.com/otnt/dotfiles/master/golang.sh | /bin/bash &&\
-cd $USERHOME/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/gocode &&\
+curl -sL https://raw.githubusercontent.com/otnt/dotfiles/master/golang.sh | /bin/bash && \
+cd $USERHOME/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/gocode && \
 go build
 
 # JS support
-curl -sL https://raw.githubusercontent.com/otnt/dotfiles/master/node.sh | /bin/bash &&\
-cd $USERHOME/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/tern_runtime &&\
+curl -sL https://raw.githubusercontent.com/otnt/dotfiles/master/node.sh | /bin/bash && \
+cd $USERHOME/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/tern_runtime && \
 npm install --production
 
 # update plugins
