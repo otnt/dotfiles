dbc8a5cb9b8950fc4405cb0432fe23cb40b7c26f
OTNT <otnt@users.noreply.github.com>
2016-04-06 23:55:43 -0400

    Update node.sh

--- install/node.sh
+++ install/node.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
+Green='\033[0;32m'
+Purple='\033[0;35m'
+NC='\033[0m' # No Color
+
 # Install Node.js, npm, need sudo
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - &&\
-apt-get install -y nodejs build-essential &&\
+curl -sL https://deb.nodesource.com/setup_5.x | /bin/bash && \
+apt-get install -y -qq nodejs build-essential &&\
+echo -e "${Green}npm installed successfully${NC}" && \
 # Update npm
-npm install npm -g &&\
+npm install npm -g && \
 # Update nodejs
-npm cache clean -f &&\
-npm install -g n &&\
-n stable
+npm cache clean -f && \
+npm install -g n && \
+n stable && \
+echo -e "${Green}npm, node, js updated successfully${NC}"
