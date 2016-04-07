9c671e2e09c3ac79fa2925e34d38b25ac394c63f
OTNT <otnt@users.noreply.github.com>
2016-04-06 23:37:47 -0400

    Update docker.sh

--- install/docker.sh
+++ install/docker.sh
@@ -1,10 +1,10 @@
 # install docker engine
-apt-get update && \
-apt-get install curl && \
+apt-get update -y -qq && \
+apt-get install curl -y -qq && \
 curl -fsSL https://get.docker.com/ | sh && \
 echo "docker engine installed successfully"
 
 # install docker machine
-curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
+curl -sL https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
 chmod +x /usr/local/bin/docker-machine && \
 echo "docker machine installed successfully"
