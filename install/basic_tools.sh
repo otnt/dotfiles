68ac35b88cb1383f9af621256b7ec8dae0a5f0ef
OTNT <otnt@users.noreply.github.com>
2016-04-06 23:36:08 -0400

    Update basic_tools.sh

--- install/basic_tools.sh
+++ install/basic_tools.sh
@@ -1,4 +1,5 @@
 # install basic tools
-apt-get update -y && \
-apt-get install -y build-essential cmake git python-dev python3-dev wget software-properties-common
+apt-get update -y -qq && \
+apt-get install -y -qq build-essential cmake git python-dev python3-dev wget software-properties-common && \
+echo "Basic tools installed: cmake, git, python, wget, other essential tools"
 
